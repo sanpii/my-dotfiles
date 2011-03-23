caac37b9f3b13e4a47ac5c66d473b900ae8bb055
sanpi <sanpi@homecomputing.fr>
2011-03-23 20:51:31 +0100

    Amélioration script installation

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
 home=$HOME;
-cwd=$(dirname $0);
 scriptname=$(basename $0);
+cwd=$(dirname $0);
+if [ "$cwd" = "." ]; then
+    cwd=$(pwd);
+fi
 
 for file in *; do
-  if [ "$file" != "$scriptname" ]; then
-    ln -s "$cwd/$file" "$home/.$file"
-  fi
+    if [ "$file" != "$scriptname" ]; then
+        if [ ! -e "$home/.$file" ]; then
+            ln -s "$cwd/$file" "$home/.$file";
+        else
+            echo "$home/.$file existe déjà";
+        fi;
+    fi;
 done;
 
