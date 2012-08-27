5448e8546b4c2097ee3ebf966f12e52476837907
Sanpi <sanpi@homecomputing.fr>
2012-08-27 18:35:19 +0200

    [awesome] Custom theme

---
+++ config/awesome/themes/cool-blue.lua
@@ -0,0 +1,16 @@
+theme = {}
+
+theme.bg_normal     = "#272C30"
+theme.bg_focus      = "#7985A3"
+theme.bg_urgent     = "#A36666"
+
+theme.fg_normal     = "#707678"
+theme.fg_focus      = "#D7DFE5"
+theme.fg_urgent     = "#D7DFE5"
+
+theme.border_width  = "1"
+theme.border_normal = "#272C30"
+theme.border_focus  = "#7985A3"
+theme.border_marked = "#A3BA8C"
+
+return theme
