47babcb58a32e8273f041aa7b88f384fcd3147fd
Sanpi <sanpi@homecomputing.fr>
2012-08-27 10:24:17 +0200

    [awesome] Reduce available layouts

--- config/awesome/layouts.lua
+++ config/awesome/layouts.lua
@@ -1,14 +1,6 @@
 layouts = {
-    awful.layout.suit.floating,
-    awful.layout.suit.tile,
-    awful.layout.suit.tile.left,
-    awful.layout.suit.tile.bottom,
-    awful.layout.suit.tile.top,
-    awful.layout.suit.fair,
-    awful.layout.suit.fair.horizontal,
-    awful.layout.suit.spiral,
-    awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.max,
-    awful.layout.suit.max.fullscreen,
-    awful.layout.suit.magnifier
+    awful.layout.suit.max, -- 1
+    awful.layout.suit.tile.right, -- 2
+    awful.layout.suit.tile.bottom, -- 3
+    awful.layout.suit.magnifier -- 4
 }
