33613f0328ab346170e895e77d02856ebe354906
Sanpi <sanpi@homecomputing.fr>
2020-01-24 20:14:50 +0100

    [firefox] Adds script to update user.js

---
+++ mozilla/firefox/user-overrides.js
@@ -0,0 +1,85 @@
+/* 0101 */
+user_pref("browser.laterrun.enabled", true);
+/* 0102 */
+user_pref("browser.startup.page", 3);
+/* 0204 */
+user_pref("general.useragent.locale", "fr-FR");
+/* 0205 */
+user_pref("intl.locale.requested", "");
+/* 0801 */
+user_pref("keyword.enabled", true);
+/* 0804 */
+user_pref("browser.sessionhistory.max_entries", 50);
+/* 0850a */
+user_pref("browser.urlbar.autocomplete.enabled", true);
+user_pref("browser.urlbar.suggest.history", true);
+user_pref("browser.urlbar.suggest.bookmark", true);
+user_pref("browser.urlbar.suggest.openpage", true);
+/* 0850c */
+user_pref("browser.urlbar.autoFill", true);
+user_pref("browser.urlbar.autoFill.typed", true);
+/* 0860 */
+user_pref("browser.formfill.enable", true);
+/* 0864 */
+user_pref("dom.forms.datetime", true);
+/* 1020 */
+user_pref("browser.sessionstore.max_tabs_undo", 20);
+user_pref("browser.sessionstore.max_windows_undo", 3);
+/* 1021 */
+user_pref("browser.sessionstore.resume_from_crash", true);
+/* 1211 */
+user_pref("security.OCSP.enabled", 0);
+/* 1212 */
+user_pref("security.OCSP.require", false);
+/* 1401 */
+user_pref("browser.display.use_document_fonts", 1);
+/* 1405 */
+user_pref("gfx.downloadable_fonts.woff2.enabled", true);
+/* 1820 */
+user_pref("media.gmp-provider.enabled", true);
+user_pref("media.gmp.trial-create.enabled", true);
+user_pref("media.gmp-manager.updateEnabled", true);
+/* 1825 */
+user_pref("media.gmp-widevinecdm.visible", true);
+user_pref("media.gmp-widevinecdm.enabled", true);
+/* 1830 */
+user_pref("media.eme.enabled", true);
+/* 2010 */
+user_pref("webgl.disabled", false);
+user_pref("webgl.min_capability_mode", false);
+user_pref("webgl.disable-extensions", false);
+/* 2012 */
+user_pref("webgl.enable-webgl2", false);
+/* 2030 */
+user_pref("media.autoplay.default", 2);
+/* 2418 */
+user_pref("full-screen-api.enabled", true);
+/* 2422 */
+user_pref("javascript.options.wasm", true);
+/* 2508 */
+user_pref("layers.acceleration.disabled", false);
+/* 2603 */
+user_pref("browser.download.useDownloadDir", true);
+/* 2662 */
+user_pref("browser.download.forbid_open_with", false);
+/* 2701 */
+user_pref("network.cookie.cookieBehavior", 1);
+/* 2802 */
+user_pref("privacy.sanitize.sanitizeOnShutdown", false);
+/* 2803 */
+user_pref("privacy.clearOnShutdown.cache", false);
+user_pref("privacy.clearOnShutdown.cookies", false);
+user_pref("privacy.clearOnShutdown.downloads", true);
+user_pref("privacy.clearOnShutdown.formdata", false);
+user_pref("privacy.clearOnShutdown.history", false);
+user_pref("privacy.clearOnShutdown.offlineApps", false);
+user_pref("privacy.clearOnShutdown.sessions", false);
+user_pref("privacy.clearOnShutdown.siteSettings", false);
+/* 4501 */
+user_pref("privacy.resistFingerprinting", false);
+/* 4504 */
+user_pref("privacy.resistFingerprinting.letterboxing", false);
+/* 5003 */
+user_pref("browser.tabs.closeWindowWithLastTab", false);
+/* 5021d */
+user_pref("browser.tabs.loadDivertedInBackground", true);
