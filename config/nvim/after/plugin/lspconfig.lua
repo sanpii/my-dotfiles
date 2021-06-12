d0d585c9abe172cacb1dd5c148aa122b3c391dce
Sanpi <sanpi@homecomputing.fr>
2021-06-12 08:49:59 +0200

    [nvim] Improves lsp configuration

--- config/nvim/after/plugin/lspconfig.lua
+++ config/nvim/after/plugin/lspconfig.lua
@@ -3,17 +3,35 @@ local lspconfig = require('lspconfig')
 local on_attach = function(client, bufnr)
     local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
     local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+    local map = vim.api.nvim_set_keymap
 
-    local opts = { noremap=true, silent=true }
+    local opts = { noremap = true, silent = true }
 
     buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-    buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    buf_set_keymap('n', '<f2>', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    buf_set_keymap('n', '<f5>', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+    buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+    buf_set_keymap('n', '<f2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+    buf_set_keymap('n', '<f5>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+
+    map('n', '<leader>«', '<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>', opts)
+    map('n', '<leader>»', '<cmd>lua vim.lsp.diagnostic.goto_next()<cr>', opts)
 end
 
 for _, lsp in ipairs({"rust_analyzer"}) do
-    lspconfig[lsp].setup{ on_attach = on_attach }
+    lspconfig[lsp].setup({ on_attach = on_attach })
 end
+
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+        virtual_text = false
+    }
+)
+
+vim.fn.sign_define('LspDiagnosticsSignError', { text = "", texthl = "LspDiagnosticsDefaultError" })
+vim.fn.sign_define('LspDiagnosticsSignWarning', { text = "", texthl = "LspDiagnosticsDefaultWarning" })
+vim.fn.sign_define('LspDiagnosticsSignInformation', { text = "", texthl = "LspDiagnosticsDefaultInformation" })
+vim.fn.sign_define('LspDiagnosticsSignHint', { text = "", texthl = "LspDiagnosticsDefaultHint" })
+
+vim.cmd('autocmd CursorHold * lua vim.lsp.diagnostic.show_line_diagnostics()')
