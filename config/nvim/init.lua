b8436c7353be362280c33376b2608a401f17bc4d
Sanpi <sanpi@homecomputing.fr>
2021-12-08 11:30:09 +0100

    [nvim] Hides eob

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -35,6 +35,7 @@
     vim.o.listchars='tab:»·,trail:·,precedes:…,extends:…,nbsp:‗'
     vim.o.showbreak='↪'
     vim.o.scrolloff=3
+    vim.opt.fillchars:append {eob = ' '}
 
     -- highlight NonText gui=bold guifg=darkgrey
     -- highlight SpecialKey gui=bold guifg=darkgrey
