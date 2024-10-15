eb1bb4af574d6afe03ac29033eb731e6caaa2bda
Sanpi <sanpi@homecomputing.fr>
2024-10-15 15:54:58 +0200

    [nvim] Improves completion style

--- config/nvim/after/plugin/cmp.lua
+++ config/nvim/after/plugin/cmp.lua
@@ -23,6 +23,30 @@ cmp.setup({
     experimental = {
         ghost_text = true,
     },
+    window = {
+        completion = {
+            scrollbar = false,
+            winhighlight = "Normal:Normal,CursorLine:Visual,Search:None,FloatBorder:Normal",
+            border = "single",
+            col_offset = -3,
+            side_padding = 0,
+        },
+
+        documentation = {
+            border = "single",
+            winhighlight = "Normal:Normal,FloatBorder:Normal",
+        },
+    },
+    formatting = {
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, vim_item)
+            local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+            local strings = vim.split(kind.kind, "%s", { trimempty = true })
+            kind.kind = " " .. (strings[1] or "") .. " "
+
+            return kind
+        end,
+    },
 })
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
--- config/nvim/after/plugin/lspkind.lua
+++
@@ -1,4 +0,0 @@
-require('lspkind').init({
-    mode = 'symbol',
-    preset = 'codicons',
-})
