e48331ad2c8e16bedc20f98afc2f30b169e70d40
Sanpi <sanpi@homecomputing.fr>
2025-03-19 10:07:38 +0100

    [nvim] Adds more dial patterns

--- config/nvim/after/plugin/dial.lua
+++ config/nvim/after/plugin/dial.lua
@@ -2,6 +2,13 @@ local augend = require('dial.augend');
 
 require("dial.config").augends:register_group{
     default = {
+        augend.integer.alias.decimal,
+        augend.integer.alias.hex,
+        augend.date.alias["%Y/%m/%d"],
+        augend.date.alias["%Y-%m-%d"],
+        augend.date.alias["%m/%d"],
+        augend.date.alias["%H:%M"],
+        augend.constant.alias.ja_weekday_full,
         augend.constant.new {
             elements = {"true", "false"},
             cyclic = true,
