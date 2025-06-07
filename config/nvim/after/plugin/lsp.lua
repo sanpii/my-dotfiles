6e8c62bc4f675ebcacead7af0e74200ee7039cdc
Sanpi <sanpi@homecomputing.fr>
2025-06-07 12:38:51 +0200

    [nvim] Removes linehl

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -29,11 +29,5 @@ vim.diagnostic.config({
             [vim.diagnostic.severity.INFO] = ' ',
             [vim.diagnostic.severity.HINT] = '󰠠 ',
         },
-        linehl = {
-            [vim.diagnostic.severity.ERROR] = 'DiagnosticSignError',
-            [vim.diagnostic.severity.WARN] = 'DiagnosticSignWarn',
-            [vim.diagnostic.severity.INFO] = 'DiagnosticSignInfo',
-            [vim.diagnostic.severity.HINT] = 'DiagnosticSignHint',
-        },
     },
 })
